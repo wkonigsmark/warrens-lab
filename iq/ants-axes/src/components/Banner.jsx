2cf4a3f778eac2058366952f662c3d71c85c6d76
wkonigsmark <wkonigsmark@gmail.com>
2026-05-19 12:54:20 -0400

    public folder for banner

--- iq/ants-axes/src/components/Banner.jsx
+++ iq/ants-axes/src/components/Banner.jsx
@@ -2,7 +2,7 @@ export default function Banner() {
   return (
     <div className="w-full bg-gradient-to-r from-purple-600 to-blue-600 shadow-lg">
       <img
-        src="/assets/banner-ants-axes.png"
+        src="/banner-ants-axes.png"
         alt="Ants & Axes Banner"
         className="w-full h-auto object-cover"
       />
